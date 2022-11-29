25fd77687d0fbce44f16985dd7f425d0f0833514
HLN-lobo <98746888+HLN-lobo@users.noreply.github.com>
2022-11-29 11:21:43 -0300

    interface-pronta

--- App.py
+++ App.py
@@ -11,7 +11,8 @@ from webbrowser import get
 from tkinter import filedialog
 from tkinter import messagebox
 import time
-import tkinter.font as tkFont
+from tkinter.font import Font 
+
 
 contatos = []
 msg = []
@@ -22,26 +23,33 @@ def PegarContato():
     contatos.append(inputContato.get())
     print(contatos)
 
+
 def ApagarContato():
     contatos.clear()
     print(contatos)
-    
+
+
 def PegarMsg():
     msg.append(inputMsg.get('1.0', 'end-1c'))
     print(msg)
 
+
 def ApagarMsg():
-    contatos.clear()
+    msg.clear()
     print(msg)
-    
+
+
 def PegarImg():
-    enviando_img = filedialog.askopenfilename(initialdir='/', title="Select a File", filetypes=(("Image files", ["*jpg*", "*png*", "*jpeg*"]), ("all files", "*-*")))
+    enviando_img = filedialog.askopenfilename(initialdir='/', title="Select a File", filetypes=(
+        ("Image files", ["*jpg*", "*png*", "*jpeg*"]), ("all files", "*-*")))
     imagens.append(enviando_img)
     print(imagens)
+
 
 def ApagarImg():
     imagens.clear()
     print(imagens)
+
 
 def Enviar():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -63,15 +71,15 @@ def Enviar():
         slp(1.5)
 
     def SendImg(imagens):
-       
-        driver.find_element(By.CSS_SELECTOR, "span[data-icon='clip']").click()     
+
+        driver.find_element(By.CSS_SELECTOR, "span[data-icon='clip']").click()
         attach = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
         attach.send_keys(imagens)
         time.sleep(2)
-        send = driver.find_element(By.XPATH, '//div[contains(@class, "_165_h _2HL9j")]')
-        send.click()    
-        
-        
+        send = driver.find_element(
+            By.XPATH, '//div[contains(@class, "_165_h _2HL9j")]')
+        send.click()
+
     for contato in contatos:
         SearchContatos(contato)
         SendMsg(msg)
@@ -80,33 +88,42 @@ def Enviar():
 
 janela = Tk()
 
-janela.title("ChatBOT")
-janela.geometry("800x600")
+janela.title("ChatBot")
+janela.configure(background='gray')
+janela.resizable(True, True)
+janela.geometry("1080x860")
+janela.maxsize(width=1080, height=860)
+
+fontStyle = Font(family="Poplar Std", size=12, weight="normal", underline=0, overstrike=0)
 
 textlabel = Label(
-    janela, text="Digite o nome ou o número de um contato ou grupo").place(x=50, y=100)
+    janela, text="Digite o nome ou o número \n de um contato ou grupo", font=fontStyle).place(x=100, y=5, width=200)
 inputContato = Entry(janela)
-inputContato.place(x=50, y=200)
-buttonContato = Button(janela, text="10 e faixa", command=PegarContato)
-buttonContato.place(x=100, y=300)
+inputContato.place(x=100, y=55, width=200, height=25)
 
-removerContato = Button(janela, text="Apagar contatos", command=ApagarContato)
-removerContato.place(x=100, y=320)
+
+buttonContato = Button(
+    janela, text="Adicione o contato ou grupo", font=fontStyle, command=PegarContato)
+buttonContato.place(x=100, y=85, width=200)
+ListaContato = LabelFrame(janela)
+ListaContato.place(x=100, y=120, height=450, width=200, )
+removerContato = Button(janela, text="Apagar contatos", font=fontStyle, command=ApagarContato)
+removerContato.place(x=100, y=580)
 
 inputMsg = Text(janela)
-inputMsg.place(x=100, y=350, width=300, height=450)
-buttonMsg = Button(janela, text="Adicionar a mensagem", command=PegarMsg)
-buttonMsg.place(x=100, y=400)
+inputMsg.place(x=400, y=120, width=600, height=450)
+buttonMsg = Button(janela, text="Adicionar a mensagem abaixo!", font=fontStyle, command=PegarMsg)
+buttonMsg.place(x=400, y=85)
 
-removerMsg = Button(janela, text="Apagar Mensagem", command=ApagarMsg)
-removerMsg.place(x=100, y=420)
+removerMsg = Button(janela, text="Apagar Mensagem",font=fontStyle, command=ApagarMsg)
+removerMsg.place(x=705, y=580)
 
-buttonImg = Button(janela, text="adionar imagem", command=PegarImg)
-buttonImg.place(x=100 , y=  450)
-buttonEnviar = Button(janela, text="Enviar", command=Enviar)
-buttonEnviar.place(x=100, y=480)
+buttonImg = Button(janela, text="Adicionar imagem", font=fontStyle, command=PegarImg)
+buttonImg.place(x=400, y=580)
+buttonEnviar = Button(janela, text="Enviar", font=fontStyle, command=Enviar)
+buttonEnviar.place(x=942, y=580)
 
-removerImg = Button(janela, text="Apagar Imagens", command=ApagarImg)
-removerImg.place(x=100, y=500)
+removerImg = Button(janela, text="Apagar Imagens", font=fontStyle, command=ApagarImg)
+removerImg.place(x=560, y=580)
 
 janela.mainloop()
